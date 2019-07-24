ef0678c37fff49236b8b3b89ebf9d4649248b1c9
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-24 14:31:38 +0400

    Create Task2.sql

---
+++ Task2.sql
@@ -0,0 +1,13 @@
+    /*
+    Instructions
+
+    Display the departments into which no employee joined in last two years.
+    Son 2 ilde hec bir yeni iscisi olmayan departamentlerin siyahisini(id, ad) cixarin.
+    */
+
+
+select d.department_name,d.department_id,e.hire_date
+from employees e right join departments d on e.department_id = d.department_id and extract(year from e.hire_date) >= (extract(year from sysdate)-2) 
+group by d.department_name,d.department_id,e.hire_date
+having e.hire_date is null
+/
