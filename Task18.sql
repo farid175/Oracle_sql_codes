9da77e83f99dd964719fc0bc65779da54824dc9b
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-24 22:19:09 +0400

    Create Task18.sql

---
+++ Task18.sql
@@ -0,0 +1,15 @@
+/*
+Display country name, city, and number of departments where department has more than 5 employees.
+
+5-den cox iscisi olan departamentlerin yerlesdiyi olke adi, seher adi ve departament sayini cixarin
+*/
+
+
+
+select count(d.department_id)departments,loc.city,c.country_name
+from employees e join departments d on e.department_id = d.department_id
+join locations loc on loc.location_id = d.location_id
+join countries c on c.country_id = loc.country_id
+group by loc.city,c.country_name
+having count(e.employee_id) >= 5
+/
