ed94e955a93fd9a731e959b5e714839a8d48c352
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-29 14:33:20 +0400

    Update Task2.sql

--- Task2.sql
+++ Task2.sql
@@ -5,9 +5,8 @@
     Son 2 ilde hec bir yeni iscisi olmayan departamentlerin siyahisini(id, ad) cixarin.
     */
 
-
 select d.department_name,d.department_id,e.hire_date
 from employees e right join departments d on e.department_id = d.department_id and extract(year from e.hire_date) >= (extract(year from sysdate)-2) 
-group by d.department_name,d.department_id,e.hire_date
-having e.hire_date is null
+group by d.department_name,d.department_id,e.hire_date,d.manager_id
+having e.hire_date is null and d.manager_id is not null
 /
