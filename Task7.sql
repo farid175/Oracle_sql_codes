e3d11c1141c451f26f2b3044618386c055ebec5c
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-29 16:52:43 +0400

    Update Task7.sql

--- Task7.sql
+++ Task7.sql
@@ -6,7 +6,8 @@ Hal hazirda 12 min ve daha cox maas alan iscilerin evvel tutduqlarin vezifelerin
 
 
 
-select j.job_id,e.first_name,e.last_name,j.job_title,e.salary
+select e.employee_id,e.first_name,e.last_name,j.job_title,e.salary
 from employees e join jobs j on e.job_id = j.job_id
+join job_history jh on e.employee_id = jh.employee_id
 where e.salary >= 12000
 /
