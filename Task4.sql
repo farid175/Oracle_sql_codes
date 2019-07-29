621a7b5c903a797f6f9674eaa67640a79cfbd46f
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-29 16:16:38 +0400

    Update Task4.sql

--- Task4.sql
+++ Task4.sql
@@ -4,8 +4,9 @@
     */
 
 
-select d.department_id,d.department_name,count(e.employee_id)employees
-from employees e join departments d on e.manager_id = d.manager_id
-group by d.department_id,d.department_name
+select em.department_id, d.department_name, count(e.employee_id) employees
+from employees e join employees em on e.manager_id = em.employee_id
+    join departments d on d.department_id = em.department_id
+group by em.department_id,d.department_name
 having count(e.employee_id) > 5
 /
