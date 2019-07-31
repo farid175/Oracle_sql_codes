2018dcc174b19fed2b83403ad0781f1308819299
farid175 <50070768+farid175@users.noreply.github.com>
2019-07-31 13:42:03 +0400

    Update Task18.sql

--- Task18.sql
+++ Task18.sql
@@ -6,10 +6,10 @@ Display country name, city, and number of departments where department has more 
 
 
 
-select count(d.department_id)departments,loc.city,c.country_name
+select loc.city,c.country_name,count(distinct d.department_id)departments
 from employees e join departments d on e.department_id = d.department_id
 join locations loc on loc.location_id = d.location_id
 join countries c on c.country_id = loc.country_id
 group by loc.city,c.country_name
-having count(e.employee_id) >= 5
+having count(e.employee_id) > 5
 /
